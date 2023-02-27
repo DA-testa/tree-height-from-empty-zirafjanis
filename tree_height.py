7df22961ef04f33461b17c2d0f95e642043b2231
Jānis Bērziņš <125191092+zirafjanis@users.noreply.github.com>
2023-02-27 14:39:44 +0200

    parbaude

--- tree_height.py
+++ tree_height.py
@@ -1,20 +1,33 @@
-n = int(input())
-parents = list(map(int, input().split()))
+import sys
+import threading
 
-tree = [[] for _ in range(n)]
-for i in range(n):
+
+def compute_height(n, parents):
+  tree = [[] for _ in range(n)]
+  for i in range(n):
     if parents[i] != -1:
-        tree[parents[i]].append(i)
+      tree[parents[i]].append(i)
 
-def dfs(node, depth):
+  def dfs(node, depth):
     if not tree[node]:
-        return depth
+      return depth
     max_depth = depth
     for child in tree[node]:
-        child_depth = dfs(child, depth + 1)
-        max_depth = max(max_depth, child_depth)
+      child_depth = dfs(child, depth + 1)
+      max_depth = max(max_depth, child_depth)
     return max_depth
 
-root = parents.index(-1)
-height = dfs(root, 0)
-print(height)
+  root = parents.index(-1)
+  height = dfs(root, 0)
+  return height
+
+
+def main():
+  n = int(input())
+  parents = list(map(int, input().split()))
+  print(compute_height(n, parents))
+
+
+sys.setrecursionlimit(10**7)  
+threading.stack_size(2**27)  
+threading.Thread(target=main).start()
